f43acc73a5fe4c32262259118d4aa8a1280b7cd7
Ruben Dario Cordoba Cuellar <ruben0793@hotmail.com>
2019-07-13 07:00:35 -0500

    Delete sql ver productos NO comprados.sql

--- ARCA DE NOE/sql ver productos NO comprados.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-c.CodigoProducto,p.Nombre,M.Nombre ModoVenta 
-FROM Producto p
-LEFT JOIN Compras c ON c.CodigoProducto = p.ID
-INNER JOIN ModoVenta M ON M.ID = P.ModoVenta
-WHERE  c.CodigoProducto IS NULL
-GROUP  BY p.ID,CodigoProducto,P.Nombre,M.Nombre,P.ModoVenta
-ORDER BY P.ModoVenta
