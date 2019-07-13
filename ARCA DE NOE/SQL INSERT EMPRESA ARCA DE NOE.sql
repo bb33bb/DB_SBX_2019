25bba6e887cb45568bc4c51312b780a45ab47033
Ruben Dario Cordoba Cuellar <ruben0793@hotmail.com>
2019-07-13 06:59:38 -0500

    Delete SQL INSERT EMPRESA ARCA DE NOE.sql

--- ARCA DE NOE/SQL INSERT EMPRESA ARCA DE NOE.sql
+++
@@ -1,7 +0,0 @@
---Registro compañia ARCA DE NOE
-TRUNCATE TABLE EMPRESA
-GO
-INSERT INTO Empresa
-SELECT
-DNI,RazonSocial,'Palmira',Direccion,Telefono,Celular,Email,SitioWeb,'',Impresora,Logo,0,10000,'',100,'F' 
-FROM SBX_ESTANDAR.dbo.COMPANIA
