90201ba7a28966a48c420a94110295aa4de2fec4
ruben <ruben0793@hotmail.com>
2019-08-18 16:44:49 -0500

    Modificacion a sp de consulta de producto de ventas

--- CAMBIOS.sql
+++ CAMBIOS.sql
@@ -413,3 +413,232 @@ GROUP BY
 ,k1.Sobres
 ,k1.ModoVenta
 END
+
+GO
+
+ALTER PROCEDURE  [dbo].[sp_consultar_producto]
+	@v_tipo_consulta AS VARCHAR(200),
+	@v_buscar AS VARCHAR(200),
+	@Item AS INT,
+	@Referencia AS VARCHAR(20),
+	@Codigo_barras AS VARCHAR(100)
+	AS
+	DECLARE 
+	@v_variable AS INT
+
+	IF(@v_tipo_consulta = 'Validar_referencia')
+	BEGIN
+	SELECT p.Item,p.Referencia,p.Nombre,p.Descripcion,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+	P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+	m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+	p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+	p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+	p.Stock_minimo,p.Stock_maximo
+	FROM Producto p
+	INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+	INNER JOIN Estado est ON est.Codigo = p.Estado
+	INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+	INNER JOIN Marca m ON m.Codigo = p.Marca
+	INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+	INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+	WHERE p.Referencia = @Referencia AND p.Estado = 1
+	END
+
+	IF(@v_tipo_consulta = 'Validar_codigo_barras')
+	BEGIN
+	SELECT p.Item,p.Referencia,p.Nombre,p.Descripcion,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+	P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+	m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+	p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+	p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+	p.Stock_minimo,p.Stock_maximo
+	FROM Producto p
+	INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+	INNER JOIN Estado est ON est.Codigo = p.Estado
+	INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+	INNER JOIN Marca m ON m.Codigo = p.Marca
+	INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+	INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+	WHERE p.CodigoBarras = @Codigo_barras AND p.Estado = 1
+	END
+	
+	IF(@v_tipo_consulta = 'Validar_Item')
+	BEGIN
+	SELECT p.Item,p.Referencia,p.Descripcion,p.Nombre,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+	P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+	m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+	p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+	p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+	p.Stock_minimo,p.Stock_maximo
+	FROM Producto p
+	INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+	INNER JOIN Estado est ON est.Codigo = p.Estado
+	INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+	INNER JOIN Marca m ON m.Codigo = p.Marca
+	INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+	INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+	WHERE p.Item = @Item AND p.Estado = 1
+	END
+	
+	IF(@v_tipo_consulta = 'Buscar_consecutivo_item')
+	BEGIN
+	SELECT ISNULL(MAX(Item),0) + 1 item FROM Producto WHERE Estado = 1
+	END
+	
+	IF(@v_tipo_consulta = 'Buscar_nombre')
+	BEGIN
+	SELECT Nombre FROM Producto
+	WHERE Nombre LIKE @v_buscar+'%' AND Estado = 1
+	GROUP BY Nombre
+	END
+	
+	IF(@v_tipo_consulta = 'Buscar_data_producto')
+	BEGIN
+	SELECT p.Item,p.Referencia,p.Descripcion,p.Nombre,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+	P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+	m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+	p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+	p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+	p.Stock_minimo,p.Stock_maximo,
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = p.Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = p.Item) Stock,
+	((SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = p.Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = p.Item)) * p.SubCantidad stockSubcantidad,
+	((SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = p.Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = p.Item)) * p.Sobres stockSobres
+	,prv.Nombre Nom_proveedor,p.DescuentoProveedor ,p.FechaVencimiento
+	FROM Producto p
+	INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+	INNER JOIN Estado est ON est.Codigo = p.Estado
+	INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+	INNER JOIN Marca m ON m.Codigo = p.Marca
+	INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+	INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+	INNER JOIN Proveedor prv ON prv.DNI = p.Proveedor
+	WHERE (p.Item LIKE @v_buscar+'%' OR p.Referencia LIKE @v_buscar+'%' OR p.CodigoBarras LIKE @v_buscar+'%' OR p.Nombre LIKE @v_buscar+'%') 
+	END
+	
+	IF(@v_tipo_consulta = 'Buscar_data_producto_exacto_item')
+	BEGIN
+	SELECT p.Item,p.Referencia,p.Descripcion,p.Nombre,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+	P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+	m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.Costo,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+	p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+	p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+	p.Stock_minimo,p.Stock_maximo,
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = @Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = @Item) Stock,
+	((SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = @Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = @Item)) * p.SubCantidad stockSubcantidad,
+	((SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = @Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = @Item)) * p.Sobres stockSobres
+	,prv.Nombre Nom_proveedor,p.DescuentoProveedor ,ISNULL((SELECT MIN(fv.FechaVecimiento) FROM FechasVencimiento fv WHERE fv.Item = p.Item ),'') FechaVencimiento
+	FROM Producto p
+	INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+	INNER JOIN Estado est ON est.Codigo = p.Estado
+	INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+	INNER JOIN Marca m ON m.Codigo = p.Marca
+	INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+	INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+	INNER JOIN Proveedor prv ON prv.DNI = p.Proveedor
+	WHERE p.Item =  @Item
+	END
+	
+	IF(@v_tipo_consulta = 'Buscar_data_producto_exacto')
+	BEGIN
+	SET @v_variable = (SELECT ISNUMERIC(@v_buscar))
+	IF(@v_variable = 1)
+	BEGIN
+	IF(CONVERT(numeric,@v_buscar) < 2147483647)
+	BEGIN
+		SELECT p.Item,p.Referencia,p.Descripcion,p.Nombre,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+		P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+		m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+		p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+		p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+		p.Stock_minimo,p.Stock_maximo,p.DescuentoProveedor
+		FROM Producto p
+		INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+		INNER JOIN Estado est ON est.Codigo = p.Estado
+		INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+		INNER JOIN Marca m ON m.Codigo = p.Marca
+		INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+		INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+		WHERE (p.Item = @v_buscar OR p.Referencia = @v_buscar OR p.CodigoBarras = @v_buscar OR p.Nombre = @v_buscar)
+	END
+	END
+	END
+
+	IF(@v_tipo_consulta = 'Buscar_data_producto_venta')
+	BEGIN
+	SELECT p.Item,p.Referencia,p.Descripcion,p.Nombre,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+	P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+	m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+	p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+	p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+	p.Stock_minimo,p.Stock_maximo,
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = p.Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = p.Item) Stock,
+	((SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = p.Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = p.Item)) * p.SubCantidad stockSubcantidad,
+	((SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = p.Item) -
+	(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = p.Item)) * p.Sobres stockSobres
+	,prv.Nombre Nom_proveedor,p.DescuentoProveedor ,p.FechaVencimiento
+	FROM Producto p
+	INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+	INNER JOIN Estado est ON est.Codigo = p.Estado
+	INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+	INNER JOIN Marca m ON m.Codigo = p.Marca
+	INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+	INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+	INNER JOIN Proveedor prv ON prv.DNI = p.Proveedor
+	WHERE (p.Item LIKE @v_buscar+'%' OR p.Referencia LIKE @v_buscar+'%' OR p.CodigoBarras LIKE @v_buscar+'%' OR p.Nombre LIKE @v_buscar+'%') 
+	AND p.Estado = 1
+	END
+
+	IF(@v_tipo_consulta = 'Buscar_data_producto_exacto_venta')
+	BEGIN
+	SET @v_variable = (SELECT ISNUMERIC(@v_buscar))
+	IF(@v_variable = 1)
+	BEGIN
+	IF(CONVERT(numeric,@v_buscar) < 2147483647)
+	BEGIN
+		SELECT p.Item,p.Referencia,p.Descripcion,p.Nombre,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+		P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+		m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+		p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+		p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+		p.Stock_minimo,p.Stock_maximo,p.DescuentoProveedor,
+		(SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Entrada +' AND Item = p.Item) -
+	    (SELECT ISNULL(SUM(Cantidad),0)  FROM Kardex WHERE movimiento = 'Salida -' AND Item = p.Item) Stock
+		FROM Producto p
+		INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+		INNER JOIN Estado est ON est.Codigo = p.Estado
+		INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+		INNER JOIN Marca m ON m.Codigo = p.Marca
+		INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+		INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+		WHERE (p.Item = @v_buscar OR p.Referencia = @v_buscar OR p.CodigoBarras = @v_buscar OR p.Nombre = @v_buscar) AND p.Estado = 1
+	END
+	END
+	END
+	
+	--SELECT p.Item,p.Referencia,p.Descripcion,p.Nombre,p.IVA,p.UnidadMedida,um.Nombre Nombre_Unidad_medida,p.Medida,
+	--	P.Estado,est.Nombre Nombre_Estado,p.Categoria,ct.Nombre Nombre_Categoria,P.Marca,
+	--	m.Nombre Nombre_Marca,p.Proveedor,p.ModoVenta,p.Cantidad,p.CostoCalculado,p.PrecioVenta,p.CodigoBarras,
+	--	p.SubCantidad,p.ValorSubcantidad,p.Sobres,p.ValorSobre,p.Foto,
+	--	p.movimiento,p.Ubicacion,ub.Nombre Nombre_ubicacion,p.Salida_para,slp.Nombre Nombre_Salida_para,
+	--	p.Stock_minimo,p.Stock_maximo,p.DescuentoProveedor
+	--	FROM Producto p
+	--	INNER JOIN UnidadMedida um ON um.Codigo = p.UnidadMedida
+	--	INNER JOIN Estado est ON est.Codigo = p.Estado
+	--	INNER JOIN Categoria ct ON ct.Codigo = p.Categoria
+	--	INNER JOIN Marca m ON m.Codigo = p.Marca
+	--	INNER JOIN Ubicacion ub ON ub.Codigo = p.Ubicacion
+	--	INNER JOIN SalidaPara slp ON slp.Codigo = p.Salida_para
+	--	WHERE p.Referencia = @v_buscar OR p.CodigoBarras = @v_buscar OR p.Nombre = @v_buscar AND p.Estado = 1
+
+	
+
+
+
